d399eb64b93e203efec484e60b4ffef3daa90969
Tino Ehrich <tino@bigpun.me>
2015-08-30 22:17:16 +0200

    fixed get request; added response header and last url in case of 301

--- src/Request.php
+++ src/Request.php
@@ -23,8 +23,13 @@ class Request
      */
     public static function get($url, array $data, array $optCustom = [])
     {
+        if (empty($data) === false)
+        {
+            $url .= '?' . http_build_query($data);
+        }
+
         $opt = [
-            CURLOPT_URL            => $url . '?' . http_build_query($data),
+            CURLOPT_URL            => $url,
             CURLOPT_RETURNTRANSFER => 1
         ];
 
@@ -73,12 +78,11 @@ class Request
      * @param array $params
      * @param int $id
      * @param array $optCustom
-     * @param string $version
      *
      * @return RequestResponse
      * @throws RequestException
      */
-    public static function jsonRpc($url, $method, array $params = [], $id = 1, array $optCustom = [], $version = '2.0')
+    public static function jsonRpc($url, $method, array $params = [], $id = 1, array $optCustom = [])
     {
         $opt = [
             CURLOPT_URL            => $url,
@@ -86,7 +90,7 @@ class Request
             CURLOPT_POST           => 1,
             CURLOPT_HTTPHEADER     => ['Content-type: application/json'],
             CURLOPT_POSTFIELDS     => json_encode([
-                'jsonrpc' => $version,
+                'jsonrpc' => '2.0',
                 'id'      => $id,
                 'method'  => $method,
                 'params'  => $params,
@@ -97,7 +101,7 @@ class Request
         $requestResponse = self::process(array_merge($opt, $optCustom));
 
         // decode json
-        $decoded = json_decode($requestResponse->getContent(), true);
+        $decoded = json_decode($requestResponse->getBody(), true);
 
         // if decoding fails throw exception with received response
         if ($decoded === null)
@@ -105,7 +109,7 @@ class Request
             throw new RequestException($requestResponse);
         }
 
-        return $requestResponse->setContent($decoded);
+        return $requestResponse->setBody($decoded);
     }
 
     /**
@@ -314,6 +318,10 @@ class Request
     {
         $curl = curl_init();
 
+        // add options to retrieve header
+        $opt[CURLOPT_VERBOSE] = 1;
+        $opt[CURLOPT_HEADER] = 1;
+
         // merge options
         foreach ($optCustom as $key => $val)
         {
@@ -325,11 +333,21 @@ class Request
         // run request
         $response = curl_exec($curl);
 
+        // parse header
+        $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        $header = self::parseHttpHeaders(substr($response, 0, $header_size));
+
+        // parse body
+        $body = substr($response, $header_size);
+
         // cache error if any occurs
         $error = curl_error($curl);
 
         // cache http code
         $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        // url on which we eventually might have ended up (301 redirects)
+        $lastUrl = curl_getinfo($curl, CURLINFO_EFFECTIVE_URL);
 
         curl_close($curl);
 
@@ -343,6 +361,29 @@ class Request
 
         return (new RequestResponse())
             ->setHttpCode($httpCode)
-            ->setContent($response);
+            ->setHeader($header)
+            ->setBody($body)
+            ->setLastUrl($lastUrl);
+    }
+
+    /**
+     * @param string $headers
+     *
+     * @return array
+     */
+    private static function parseHttpHeaders($headers)
+    {
+        $data = [];
+        $lines = explode("\r\n", chop($headers));
+
+        $data['http-status'] = array_shift($lines);
+
+        foreach ($lines as $line)
+        {
+            $parts = explode(':', $line);
+            $data[strtolower(array_shift($parts))] = join(':', $parts);
+        }
+
+        return $data;
     }
 }
--- src/RequestResponse.php
+++ src/RequestResponse.php
@@ -15,26 +15,40 @@ class RequestResponse
     private $httpCode;
 
     /**
-     * @var mixed
+     * @var array
      */
-    private $content;
+    private $header;
 
     /**
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @var string
+     */
+    private $lastUrl;
+
+    /**
+     * @deprecated
+     *
      * @return mixed
      */
     public function getContent()
     {
-        return $this->content;
+        return $this->body;
     }
 
     /**
-     * @param mixed $content
+     * @deprecated
+     *
+     * @param mixed $body
      *
      * @return RequestResponse
      */
-    public function setContent($content)
+    public function setContent($body)
     {
-        $this->content = $content;
+        $this->body = $body;
 
         return $this;
     }
@@ -55,6 +69,66 @@ class RequestResponse
     public function setHttpCode($httpCode)
     {
         $this->httpCode = $httpCode;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    /**
+     * @param array $header
+     *
+     * @return RequestResponse
+     */
+    public function setHeader(array $header)
+    {
+        $this->header = $header;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     *
+     * @return RequestResponse
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastUrl()
+    {
+        return $this->lastUrl;
+    }
+
+    /**
+     * @param string $lastUrl
+     *
+     * @return RequestResponse
+     */
+    public function setLastUrl($lastUrl)
+    {
+        $this->lastUrl = $lastUrl;
 
         return $this;
     }
